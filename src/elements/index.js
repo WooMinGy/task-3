137f5d82c47a1f99dea8c82414060106a07ea4db
WOO MINGY <alsrl1110@naver.com>
2021-11-30 18:48:41 +0900

    로그인, 회원가입, 헤더 뷰 작성

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,7 +1,9 @@
 import Grid from "./Grid";
 import Image from "./Image";
 import Text from "./Text";
+import Input from "./Input";
+import Button from "./Button";
 
 //한번에 여러개 import 하는 방법
 
-export { Grid, Image, Text };
+export { Grid, Image, Text, Input, Button };
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import styled from "styled-components";
 import { BrowserRouter, Route } from "react-router-dom";
 import PostList from "../pages/PostList";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+
+import Header from "../components/Header";
+import { Grid } from "../elements";
 
 function App() {
   return (
     <React.Fragment>
-      <BrowserRouter>
-        <Route path="/" exact component={PostList}></Route>
-      </BrowserRouter>
+      <Grid>
+        <Header></Header>
+        <BrowserRouter>
+          <Route path="/" exact component={PostList} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Signup} />
+        </BrowserRouter>
+      </Grid>
     </React.Fragment>
   );
 }
